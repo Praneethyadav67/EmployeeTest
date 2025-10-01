ffbdfcf04c9db1a9f77fade376217a5fcbae06f1
Praneethyadav67 <85743998+Praneethyadav67@users.noreply.github.com>
2025-10-01 11:30:02 +0530

    Update Substraction.java

--- Substraction.java
+++ Substraction.java
@@ -5,8 +5,7 @@ public class Substraction{
    public static void main(String[] args){
              int a = 30;
 			 int b= 10;
-			 int c = a-b;
-			 
+			 int c = a-b;	 
         System.out.println( "After Sub C value is: "+c);
    }
 }
